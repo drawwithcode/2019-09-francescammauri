16c87dc7776f846ad1659a0c4b5f5e72a7af8b23
francescammauri <>
2019-11-19 16:26:15 +0100

    Assignment 9 uploaded

--- sketch.js
+++ sketch.js
@@ -1,11 +1,61 @@
-function preload(){
-  // put preload code here
-}
+var value = 0;
+var music
+var monkey;
+var maracasa ;
+var maracasb;
+
+
+
 
 function setup() {
-  // put setup code here
+createCanvas(windowWidth,windowHeight);
+monkey = loadImage("./assets/scimmia samba.png");
+caracasa = loadImage("./assets/maracas 1-min.png");
+caracasb = loadImage("./assets/maracas 2-min.png")
+music = loadSound("./assets/maracas.wav");
+
+  setShakeThreshold(10);
+ frameRate(15);
 }
 
 function draw() {
-  // put drawing code here
+    background("yellow")
+  if (value === 0) {
+
+    imageMode(CENTER);
+    image(maracasa, width/2, height/2, maracasa.width/2, maracasa.height/2);
+    fill("white");
+    textStyle(BOLD)
+    textSize(20)
+    text('Shake it to play',windowWidth/2,250);
+    music.play();
+    music.loop();
+  } else if (value === 1) {
+    background("orange")
+    imageMode(CENTER);
+    image(maracasb ,width/2, height/2, maracasb.width/2, maracasb.height/2);
+    fill("white");
+    textStyle(BOLD)
+    textSize(20)
+    text('keep playing',windowWidth/2,250);
+  } else if (value === 2) {
+    background("red")
+    imageMode(CENTER);
+    image(monkey ,width/2, height/2, monkey.width/2, monkey.height/2);
+    fill("white");
+    textStyle(BOLD)
+    textSize(20)
+    text('Let us SAMBA',windowWidth/2,250);
+    music.stop()
+  }
+}
+
+function deviceShaken() {
+  value = 1;
+}
+
+function deviceTurned() {
+  if (turnAxis === 'X'){
+    value = 2;
+  }
 }
